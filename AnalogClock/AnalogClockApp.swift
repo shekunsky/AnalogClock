a834cf33c954cd3e408331956437b908d2074a41
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-04-21 13:10:35 +0300

    Initial Commit

---
+++ AnalogClock/AnalogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockApp.swift
+//  AnalogClock
+//
+//  Created by Alex2 on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
